71b5019c39689b577a7c73d4895a862ac6afa2f2
soswah-L <lyesuahj@gmail.com>
2025-10-14 21:00:10 +0800

    Study on C input and output

---
+++ 01CFundamentals/exercises/IO/cIO.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello cold world! \n %%");
+
+    int i;
+    i = 31;
+    printf("%d %x %o", i, i, i);
+    printf("\n %f", i);
+
+    /* Practice 1 */
+
+
+
+    return 0;
+
+}
