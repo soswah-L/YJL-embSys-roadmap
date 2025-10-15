c74bd603bfc3c77bf855324688fb10cb4a1ddcdb
soswah-L <lyesuahj@gmail.com>
2025-10-15 14:09:46 +0800

    Refresher on C input process

--- 01CFundamentals/exercises/IO/cIO.c
+++ 01CFundamentals/exercises/IO/cIO.c
@@ -4,10 +4,19 @@ int main()
 {
     printf("Hello cold world! \n %%");
 
-    int i;
+    int i, j;
+    j = 22;
     i = 31;
     printf("%d %x %o", i, i, i);
-    printf("\n %f", i);
+    printf("\n %f \n", i);
+
+    /* Common Mistakes */
+    printf("\n%d %d %d", i, j);
+    printf("\n%d", i, j);
+    
+    char c;
+    c = '?';
+    printf("\n%d", c);
 
     /* Practice 1 */
 
