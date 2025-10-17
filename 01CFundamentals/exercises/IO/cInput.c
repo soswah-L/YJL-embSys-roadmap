d58ff7a6d17fa651437f73f2ce3a6a59253215c3
soswah-L <lyesuahj@gmail.com>
2025-10-17 16:56:52 +0800

    Coding practice for C input and output with specifiers

--- 01CFundamentals/exercises/IO/cInput.c
+++ 01CFundamentals/exercises/IO/cInput.c
@@ -3,7 +3,7 @@
 
 int main(){
 
-    int MaxSheep, value, square;
+    /*int MaxSheep, value, square;
     float value1, squareroot;
 
     scanf("%d", &MaxSheep);
@@ -21,7 +21,34 @@ int main(){
         squareroot = sqrt(value1);
         printf("You have given me: %f\n", value1);
         printf("The sqaure root is: %f\n", squareroot);
-    }
+    }*/
+
+    /* Practice 1 */
+    int days;
+    float piVal;
+    
+    printf("How many days in the week: ");
+    scanf("%d", &days);
+
+    printf("The value of Pi to two points: ");
+    scanf("%f", &piVal);
+
+    printf("\nThere are %d days in the week.\n", days);
+    printf("Pi value is %f", piVal);
+
+    /* Practice 2 */
+        float val1, val2;
+    
+    printf("Enter your first value: ");
+    scanf("%f", &val1);
+    printf("\nEnter your second value: ");
+    scanf("%f", &val2);
+    
+    printf("\n\nValue A: %g", val1);
+    printf("\nValue B: %g", val2);
+    printf("\n%f + %f = %f.", val1, val2, val1 + val2);
+    printf("\n%f - %f = %f.", val1, val2, val1 - val2);
+    printf("\n%f * %f = %f.", val1, val2, val1 * val2);
 
     return 0;
 }
