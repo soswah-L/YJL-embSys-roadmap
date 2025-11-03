7bc1885df585f6ecaf4c71faebe3409c71695e45
soswah-L <lyesuahj@gmail.com>
2025-11-03 15:36:39 +0800

    Study on C while loops with program application/examples

---
+++ 01CFundamentals/exercises/controlFlow/cLoops.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void){
+        int counter = 5;
+
+    while(--counter) {
+        puts("I am an awesome program");
+    }
+
+
+    return 0;
+}
---
+++ 01CFundamentals/exercises/controlFlow/cLoopsWhile.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (void){
+
+    int number;
+    int max = -100000;
+    int counter = 0;
+
+    do {
+        scanf("%d",&number);
+        if(number != -1) 
+            counter++;
+            if(number > max)
+                max = number;
+    }
+	while (number != -1);
+		if(counter)
+          printf("The largest number is %d \n",max);
+		else 
+			printf("Hey, you haven't entered any number!");
+
+    return 0;
+}
